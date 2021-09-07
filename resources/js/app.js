7a66e386b6343d274a1e95bfdd7cc68cf7e4420e
BlueVortex123 <duicu.andrei@gmail.com>
2021-09-07 19:49:33 +0000

    add Vue and Inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    new Vue({
+      render: h => h(App, props),
+    }).$mount(el)
+  },
+})
